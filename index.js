b462ca9c9fde548b6fd162810db15d1b6c293d7e
shirotamaki <shiro.tamaki63@gmail.com>
2021-12-23 16:57:07 +0900

    jsonファイルを取得するプログラムを実装

--- index.js
+++ index.js
@@ -1,47 +1,18 @@
 #!/usr/bin/env node
-// 'use strict'
+'use strict'
 
+const Enquirer = require('enquirer')
+const fs = require('fs')
 
-// const Enquirer = require('enquirer')
-//
-// console.log('foo')
-//
-// async function callApi() {
-//   const res = await fetch("https://jsonplaceholder.typicode.com/users")
-//   const users = await Json.parse(res)
-//   console.log(users)
-// }
-// callApi()
-//
-// function main() {
-// fetch("https://jsonplaceholder.typicode.com/users")
-// .then( function (res){
-//   return res.json()
-// })
-// }
-//
-// console.log(main())
-const fetch = require('node-fetch')
-
-
-const API = "https://connpass.com/api/v1/event/"
-// console.log(API)
-async function callApi() {
-  const res = await fetch(API)
-  const json = await res.json()
-  // const api = await JSON.parse(res)
-  console.log(json)
-  // for (const i of json) {
-  //   console.log(`${i.title}`)
+function main() {
+  fs.readFile( "./oreilly_animals.json", { encoding: "utf8" }, (err, file) => {
+   if(err) {
+     console.error(err.message)
+   } else {
+     const obj = JSON.parse(file);
+     console.log(obj)
+   }
+  });
 }
 
-callApi()
-
-// function main() {
-//   fetch(API).then(function (response) {
-//     return response.text();
-//   }).then(function (text) {
-//     console.log(text)
-//   })
-// }
-// main()
+main()
