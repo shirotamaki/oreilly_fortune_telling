5b74f84ce8d3e7602a007f1be103b15c1290bf3d
shirotamaki <shiro.tamaki63@gmail.com>
2021-12-24 21:41:18 +0900

    動物取得リスト（JSONファイルをオブジェクトとして返す処理）

--- index.js
+++ index.js
@@ -4,15 +4,31 @@
 const Enquirer = require('enquirer')
 const fs = require('fs')
 
+// 動物リストを取得
 function main() {
   fs.readFile( "./oreilly_animals.json", { encoding: "utf8" }, (err, file) => {
    if(err) {
      console.error(err.message)
    } else {
-     const obj = JSON.parse(file);
+     console.log(file)
+     const obj = JSON.parse(file)  // JSON.parseすることでオブジェクトして返す
      console.log(obj)
    }
   });
 }
 
 main()
+
+
+// const fetch = require('node-fetch')
+// const getUri = () => {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//   .then(response => response.text())
+//   .then(text => {
+//     console.log(text);
+// })
+//   }
+// getUri()
+//
+
+
