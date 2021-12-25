f5492a489f26eb1f184bc6705f6fa166b0bd7b9b
shirotamaki <shiro.tamaki63@gmail.com>
2021-12-25 22:26:00 +0900

    12_25途中 質問文の作成とEnquirerの実装

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict'
 
-const Enquirer = require('enquirer')
+const Enquirer = require('enquirer');
 const fs = require('fs')
 
 // 動物リストを取得
@@ -10,25 +10,37 @@ function main() {
    if(err) {
      console.error(err.message)
    } else {
-     console.log(file)
      const obj = JSON.parse(file)  // JSON.parseすることでオブジェクトして返す
-     console.log(obj)
+     console.log(obj[0]["powers"][0])
    }
   });
 }
 
-main()
+async function quiz() {
+  fs.readFile( "./list_of_questions.json", { encoding: "utf8" }, (err, file) => {
+    if(err) {
+      console.error(err.message)
+    } else {
+      // console.log(questions)// JSON.parseすることでオブジェクトして返す
+      const prompt = new Promise((resolve) => {
+        const question = JSON.parse(file)
+        const values = {
+        type: 'select',
+        name: 'question',
+        message: "質問",
+        choices: questions
+        }
+        const answer = Enquirer.prompt(values)
+        answer.then(({ questions}) => {
+
+        })
+      })
+    }
+  })
+}
 
 
-// const fetch = require('node-fetch')
-// const getUri = () => {
-//   fetch("https://jsonplaceholder.typicode.com/users")
-//   .then(response => response.text())
-//   .then(text => {
-//     console.log(text);
-// })
-//   }
-// getUri()
-//
+quiz()
+// main()
 
 
