b49308ab7ab437fc029885e9edb12146e87fc29c
shirotamaki <shiro.tamaki63@gmail.com>
2021-12-27 11:35:36 +0900

    オライリー動物リストを作成

---
+++ oreilly_animals.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    "animal": "12-Wired Bird of Paradise",
+    "cover_src": "https://covers.oreilly.com/images/9780596155452/cat.gif",
+    "link": "https://shop.oreilly.com/product/9780596155452.do",
+    "title": "Mobile Design and Development"
+  },
+  {
+    "animal": "3-Banded Armadillo",
+    "cover_src": "https://covers.oreilly.com/images/0636920024491/cat.gif",
+    "link": "https://shop.oreilly.com/product/0636920024491.do",
+    "title": "Windows PowerShell for Developers"
+  },
+  {
+    "animal": "Aardvark",
+    "cover_src": "https://covers.oreilly.com/images/9780596007065/cat.gif",
+    "link": "https://shop.oreilly.com/product/9780596007065.do",
+    "title": "Jakarta Commons Cookbook"
+  },
+  {
+    "animal": "Aardwolf",
+    "cover_src": "https://covers.oreilly.com/images/0636920029786/cat.gif",
+    "link": "https://shop.oreilly.com/product/0636920029786.do",
+    "title": "Clojure Cookbook"
+  },
+  {
+    "animal": "Addax, aka Screwhorn Antelope",
+    "cover_src": "https://covers.oreilly.com/images/9780596804855/cat.gif",
+    "link": "https://shop.oreilly.com/product/9780596804855.do",
+    "title": "Ubuntu: Up and Running"
+  }
+]
