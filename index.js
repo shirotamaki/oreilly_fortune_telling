099f7ca4d3c7d1c879b1c200cc1e42bd3343d43b
shirotamaki <shiro.tamaki63@gmail.com>
2022-01-01 21:19:15 +0900

    01_01途中、quiz()の修正、start関数の修正

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const list_of_questions = require('./list_of_questions.js')
 const oreilly_animals = require('./oreilly_animals.js')
 const readline = require('readline')
 
-
 // function wait () {
 //     readline.emitKeypressEvents(process.stdin)
 //     process.stdin.setRawMode(true)
@@ -15,70 +14,71 @@ const readline = require('readline')
 // }
 
 // questionを引数で受けて、CLI上にquestionを表示。次に標準入力の受け付け状態になる、入力後エンターを押すと終了
-  async function readUserInput (question) {
-    const inputData = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
+async function readUserInput (question) {
+  const inputData = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  return new Promise((resolve) => {
+    inputData.question(question, (answer) => {
+      resolve(answer)
+      inputData.close()
     })
+  })
+}
 
-    return new Promise((resolve) => {
-      inputData.question(question, (answer) => {
-        resolve(answer)
-        inputData.close()
-      })
-    })
-  }
+async function greeting () {
+  console.log('クイズに答えることで、あたなにお薦めのオライリー本（動物）をお薦めするよ！')
+}
 
-  async function greeting() {
-    console.log(`クイズに答えることで、あたなにお薦めのオライリー本（動物）をお薦めするよ！`)
-  }
-
-  async function explain() {
-    console.log()
-  }
+async function explain () {
+  console.log()
+}
 
 async function start () {
   await greeting()
-  const name = await readUserInput(`まずはあなたのことを教えてね\nニックネームを入力してエンターを押してね\n`)
+  const name = await readUserInput('まずはあなたのことを教えてね\nニックネームを入力してエンターを押してね\n')
   console.log(`ありがとう、${name}さん！`)
-  const food = await readUserInput(`次はあなたの好きな食べ物を入力してね\n`)
+  const food = await readUserInput('次はあなたの好きな食べ物を入力してね\n')
   console.log(`${food}が好きなんだね！`)
-  const answer = await quiz()
-  console.log(answer)
-  await selectAnimal()
+  const answer1 = await quiz()
+  console.log(answer1)
+  const answer2 = await quiz()
+  console.log(answer2)
+  // let answer = await quiz()
+  // console.log(answer)
 }
 
 start()
 
 // クイズ用メソッド
-function quiz() {
+function quiz () {
   const random = Math.floor(Math.random() * (5 - 0)) + 0
   const question = list_of_questions[random].message
   const answers = list_of_questions[random].choices.flat()
   const correctAnswer = list_of_questions[random].correctChoice
   const prompt = new Quiz({
-    name: 'answer',     // 選択された答えのオブジェクト
-    message: question,  // 質問文
-    choices: answers,    // クイズの答えの候補のリスト
-    correctChoice: correctAnswer   //クイズの答え
+    name: 'answer', // 選択された答えのオブジェクト
+    message: question, // 質問文
+    choices: answers, // クイズの答えの候補リスト
+    correctChoice: correctAnswer // クイズの答えインデックス
   })
 
-  prompt
-  .run()
-  .then((answerObj) => {
-    if (answerObj.correct) {
-      console.log('正解！')
-    } else {
-      console.log('残念、不正解！')
-    }
-  })
-  .catch(console.error)
+  prompt.run()
+    .then((answerObj) => {
+      if (answerObj.correct) {
+        console.log('正解！')
+      } else {
+        console.log('残念、不正解！\n')
+      }
+    }).catch(console.error)
 }
 
 // quiz()
 
 // 書籍データ出力用メソッド
-function selectAnimal() {
+function selectAnimal () {
   const random = Math.floor(Math.random() * (5 - 0)) + 0
   const animal = oreilly_animals[random]
   console.log('お薦めのオライリー本は......')
--- list_of_questions.js
+++ list_of_questions.js
@@ -1,76 +1,74 @@
 module.exports = [
   {
-    "name": "name",
-    "message": "一日の睡眠時間が最も長い動物は？",
-    "choices": [["コアラ"], ["ライオン"], ["ナマケモノ"], ["キリン"]],
-    "correctChoice": 0
+    name: 'name',
+    message: '一日の睡眠時間が最も長い動物は？',
+    choices: [['コアラ'], ['ライオン'], ['ナマケモノ'], ['キリン']],
+    correctChoice: 0
   },
   {
-    "name": "name",
-    "message": "イカの血液の色は？",
-    "choices": [["赤色"], ["白色"], ["青色"], ["七色"]],
-    "correctChoice": 2
+    name: 'name',
+    message: 'イカの血液の色は？',
+    choices: [['赤色'], ['白色'], ['青色'], ['七色']],
+    correctChoice: 2
   },
   {
-  "name": "name",
-  "message": "キリンの睡眠時間は？",
-  "choices": [["20時間"], ["8時間"], ["20分"], ["寝ない"]],
-  "correctChoice": 2
+    name: 'name',
+    message: 'キリンの睡眠時間は？',
+    choices: [['20時間'], ['8時間'], ['20分'], ['寝ない']],
+    correctChoice: 2
   },
   {
-    "name": "name",
-    "message": "らくだのコブには何が詰まっている？",
-    "choices": [["脂肪"], ["水"], ["血液"], ["夢"]],
-    "correctChoice": 0
+    name: 'name',
+    message: 'らくだのコブには何が詰まっている？',
+    choices: [['脂肪'], ['水'], ['血液'], ['夢']],
+    correctChoice: 0
   },
   {
-    "name": "name",
-    "message": "爬虫類なのは？",
-    "choices": [["ヤモリ"], ["イモリ"], ["タモリ"], ["囲炉裏"]],
-    "correctChoice": 0
+    name: 'name',
+    message: '爬虫類なのは？',
+    choices: [['ヤモリ'], ['イモリ'], ['タモリ'], ['囲炉裏']],
+    correctChoice: 0
   },
   {
-    "name": "name",
-    "message": "１年で最もたくさんの人間を殺す動物は？",
-    "choices": [["ヘビ"], ["蚊"], ["ワニ"], ["ライオン"]],
-    "correctChoice": 1
+    name: 'name',
+    message: '１年で最もたくさんの人間を殺す動物は？',
+    choices: [['ヘビ'], ['蚊'], ['ワニ'], ['ライオン']],
+    correctChoice: 1
   },
   {
-    "name": "name",
-    "messagae": "いちばん長生きなのは次のうちどれ？",
-    "choices": [["サメ"], ["クジラ"], ["人間"], ["鶴"]],
-    "correctChoice": 0
+    name: 'name',
+    messagae: 'いちばん長生きなのは次のうちどれ？',
+    choices: [['サメ'], ['クジラ'], ['人間'], ['鶴']],
+    correctChoice: 0
   },
   {
-  "name": "name",
-  "messagae": "『プログラミングRust第2版』の表紙に描かれているアニマルは次のうちどれ？",
-  "choices": [["オオヒロバオウギガニ"], ["ワタリガニ"], ["ワタリドリ"], ["アレクサンドロス"]],
-  "correctChoice": 0
+    name: 'name',
+    messagae: '『プログラミングRust第2版』の表紙に描かれているアニマルは次のうちどれ？',
+    choices: [['オオヒロバオウギガニ'], ['ワタリガニ'], ['ワタリドリ'], ['アレクサンドロス']],
+    correctChoice: 0
   },
   {
-    "name": "name",
-    "messagae": "『JavaScript第7版』の表紙に描かれているアニマルは次のうちどれ？",
-    "choices": [["フラミンゴ"], ["キリン"], ["ジャワサイ"], ["ジャワカレー"]],
-    "correctChoice": 2
+    name: 'name',
+    messagae: '『JavaScript第7版』の表紙に描かれているアニマルは次のうちどれ？',
+    choices: [['フラミンゴ'], ['キリン'], ['ジャワサイ'], ['ジャワカレー']],
+    correctChoice: 2
   },
   {
-    "name": "name",
-    "messagae": "『初めてのRuby』の表紙に描かれているアニマルは次のうちどれ？",
-    "choices": [["シマウマ"], ["キリン"], ["アフリカゾウ"], ["チーター"]],
-    "correctChoice": 1
+    name: 'name',
+    messagae: '『初めてのRuby』の表紙に描かれているアニマルは次のうちどれ？',
+    choices: [['シマウマ'], ['キリン'], ['アフリカゾウ'], ['チーター']],
+    correctChoice: 1
   },
   {
-    "name": "name",
-    "messagae": "『入門 Python 3 第2版』の表紙に描かれているアニマルは次のうちどれ？",
-    "choices": [["ガラガラヘビ"], ["ニシキヘビ"], ["ハブ"], ["マングース"]],
-    "correctChoice": 1
+    name: 'name',
+    messagae: '『入門 Python 3 第2版』の表紙に描かれているアニマルは次のうちどれ？',
+    choices: [['ガラガラヘビ'], ['ニシキヘビ'], ['ハブ'], ['マングース']],
+    correctChoice: 1
   },
   {
-    "name": "name",
-    "messagae": "『入門 vi 第6版』の表紙に描かれているアニマルは次のうちどれ？",
-    "choices": [["ニホンザル"], ["メガネザル"], ["チンパンジー"], ["ラクダ"]],
-    "correctChoice": 1
+    name: 'name',
+    messagae: '『入門 vi 第6版』の表紙に描かれているアニマルは次のうちどれ？',
+    choices: [['ニホンザル'], ['メガネザル'], ['チンパンジー'], ['ラクダ']],
+    correctChoice: 1
   }
 ]
-
-
